d9a7e7a35eb7d541b10acd76be185f322acd9388
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-11-13 19:25:39 -0300

    refactor

--- answer/src/App.js
+++ answer/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="app">
+      <header className="app-header">
 
         <p>
           Descubra o quão machista opressor você é >:()
