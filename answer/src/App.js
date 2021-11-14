8dd347cbd8701cd8929ae8d1d7bd421b3cd98fd2
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-11-13 21:08:21 -0300

    create: app footer

--- answer/src/App.js
+++ answer/src/App.js
@@ -4,14 +4,18 @@ function App() {
   return (
     <div className="app">
       <header className="app-header">
-
         <p>
           Descubra o quão Machista opressor você é >:()
         </p>
-
-        
-
       </header>
+
+
+
+      <footer className="app-footer">
+        <p>
+          Equipe - | Samuel Ricardo | Milena Fernanda | Claudson |
+        </p>
+      </footer>
     </div>
   );
 }
