1ec48fd46cbdeb10ea2604ef8db2c5a2b88c92e1
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-11-13 22:04:43 -0300

    Create: field - title | description (./components/Question)

--- answer/src/components/Question/index.jsx
+++ answer/src/components/Question/index.jsx
@@ -5,7 +5,8 @@ export default (props) => {
   return (
     <div className="Question">
 
-      
+      <p className="title"> {props.title} </p>
+      <p className="description"> {props.description} </p>
 
       {props.children}
     </div>
