90991ec2758f21d477c1dc6ac749693764a7af28
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-11-13 22:04:01 -0300

    Create: Component - Question (./component)

---
+++ answer/src/components/Question/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './question.css';
+
+export default (props) => {
+  return (
+    <div className="Question">
+
+      
+
+      {props.children}
+    </div>
+  )
+}
